027c6c7a361dbd3cc0b5ae3c7fe53fc78ec4a6a6
Rashmi-AN <118734977+Rashmi-AN@users.noreply.github.com>
2023-04-14 17:28:50 +0530

    Update Average time of first response.py

--- src/main/python/Sunbird-lern overall/Community_disscusion/Discussions/Average time of first response.py
+++ src/main/python/Sunbird-lern overall/Community_disscusion/Discussions/Average time of first response.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import configparser
 
 config = configparser.ConfigParser(interpolation=None)
-config.read("config.ini")
+config.read("Please add the config file path")
 
 name_of_community = config.get("COMMUNITY_NAME", "name")
 
@@ -67,67 +67,3 @@ else:
     print("No discussions with comments found.")
 
 
-
-
-
-
-
-# import requests
-# import json
-# from datetime import datetime
-#
-# query = """
-# query ($cursor: String) {
-#   repository(owner: "sunbird-ed", name: "community") {
-#     discussions(first: 100, after: $cursor) {
-#       pageInfo {
-#         hasNextPage
-#         endCursor
-#       }
-#       nodes {
-#         id
-#         createdAt
-#         comments(first: 1) {
-#           nodes {
-#             createdAt
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-# """
-#
-# url = 'https://api.github.com/graphql'
-# headers = {"Authorization": "bearer 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"}
-#
-# cursor = None
-# count = 0
-# total_response_time = 0
-#
-# while True:
-#     variables = {'cursor': cursor}
-#     response = requests.post(url, headers=headers, json={'query': query, 'variables': variables})
-#     if response.status_code == 200:
-#         json_data = json.loads(response.text)
-#         discussions = json_data["data"]["repository"]["discussions"]["nodes"]
-#         for d in discussions:
-#             if d["comments"]["nodes"]:
-#                 posted_time = datetime.strptime(d["createdAt"], '%Y-%m-%dT%H:%M:%SZ')
-#                 response_time = datetime.strptime(d["comments"]["nodes"][0]["createdAt"], '%Y-%m-%dT%H:%M:%SZ')
-#                 total_response_time += (response_time - posted_time).total_seconds()
-#                 count += 1
-#         has_next_page = json_data["data"]["repository"]["discussions"]["pageInfo"]["hasNextPage"]
-#         if has_next_page:
-#             cursor = json_data["data"]["repository"]["discussions"]["pageInfo"]["endCursor"]
-#         else:
-#             break
-#     else:
-#         print("Request failed with status code:", response.status_code)
-#         break
-#
-# if count > 0:
-#     avg_response_time = total_response_time / count / 3600
-#     print(f"Average time of first response: {avg_response_time:.2f} seconds")
-# else:
-#     print("No discussions with comments found.")
